091e3dd365415617972e3dce9d6f207804d8dfb4
jaejun <jaejun.lee@live.com>
2019-12-27 01:32:46 -0700

    scrape salary and transfer html, and merge them

---
+++ src/webscrap/webscrap_spotrac.py
@@ -0,0 +1,88 @@
+from bs4 import BeautifulSoup
+import os
+import requests
+from requests.exceptions import HTTPError
+import os
+import errno
+import pandas as pd
+import numpy as np
+import re
+from pymongo import MongoClient
+from decimal import Decimal
+
+
+def get_soup(path):
+    '''
+    get soup from the designated html file
+    INPUT: 
+        - path: string
+    OUTPUT: 
+        - BeautifulSoup object
+    '''
+    with open(path) as f:
+        html_str = f.read()
+    return BeautifulSoup(html_str, 'lxml')
+
+def scrape_salary_html(soup):
+    '''
+    scrape salary table
+    INPUT: 
+        - soup: BeautifulSoup object for salary html
+    OUTPUT: 
+        - list of dictionary
+    '''
+
+    table = soup.find_all('table', class_="datatable noborder tablesorter tablesorter-default")[0]
+    trs = table.tbody.find_all('tr')
+    lst_players = []
+    for tr in trs:
+        dict_player = {}
+        dict_player['name'] = tr.select(".team-name")[0].text.strip()
+        dict_player['position'] = tr.select(".rank-position")[0].text.strip()
+        salary = tr.select(".rank-value")[0].text.strip()
+        dict_player['salary'] = int(salary.replace(',', '').replace('£', ''))
+        #value = Decimal(sub(r'[^\d.]', '', money))
+        #value = float(money.replace(',', '').replace('£', ''))
+        lst_players.append(dict_player)
+    
+    return lst_players
+
+def scrape_transfer_html(soup):
+    '''
+    scrape transfer value table
+    INPUT: 
+        - soup: BeautifulSoup object for transfer html
+    OUTPUT: 
+        - list of dictionary
+    '''
+
+    table = soup.find_all('table', class_="datatable noborder tablesorter tablesorter-default")[0]
+    trs = table.tbody.find_all('tr')
+    lst_players = []
+    for tr in trs:
+        dict_player = {}
+        dict_player['name'] = tr.select(".team-name")[0].text.strip()
+        dict_player['position'] = tr.select(".rank-position")[0].text.strip()
+        transfer = tr.select(".rank-value")[0].text.strip()
+        dict_player['transfer'] = int(transfer.replace(',', '').replace('£', ''))
+        #value = Decimal(sub(r'[^\d.]', '', money))
+        #value = float(money.replace(',', '').replace('£', ''))
+        lst_players.append(dict_player)
+    
+    return lst_players
+
+
+
+
+if __name__=='__main__':
+
+    soup_salary = get_soup("./data/html/salary/EPL_Salary_Rankings_Spotrac_20182019.html")
+    soup_transfer = get_soup("./data/html/salary/EPL_Transfer_Rankings_Spotrac_20182019.html")
+    
+    salaries = scrape_salary_html(soup_salary)
+    transfers = scrape_transfer_html(soup_transfer)
+
+    df_salaries = pd.DataFrame(salaries)
+    df_transfers = pd.DataFrame(transfers)
+    df = pd.merge(left=df_salaries, right=df_transfers, on=['name', 'position'], how='outer')
+
