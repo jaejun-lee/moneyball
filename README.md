9fd4ffc774a36c9c764cbdad41ed1a6b5a4018f8
jaejun <jaejun.lee@live.com>
2020-01-09 20:06:59 -0700

    the first draft of README

--- README.md
+++ README.md
@@ -1,39 +1,123 @@
+
 # Will More Spending money guarantee winning in Premier League Soccer League.
 
+*Capstone I Project for Galvanize Data Science Immersive, Week 4*
 
+*by Jaejun Lee*
 
-# Background
-----------
-Since Machester City, a English Premier League team, had been bought by Mansour bin Zayed Al Nahyan, member of the royal family of Abu Dhabi, in 2008, the team spent over £1.4 billion in player transfer (https://www.verdict.co.uk/manchester-city-trophies-sheikh-mansour/) and achieved 7 trophies including 3 premier league title. I got interested how strongly the player salary affects teams performance in winning, goals, clean sheet and etc.
+## Background and Goal
+Since Machester City, a English Premier League team, had been bought by Mansour bin Zayed Al Nahyan, member of the royal family of Abu Dhabi, in 2008, the team spent over £1.4 billion in player transfer (https://www.verdict.co.uk/manchester-city-trophies-sheikh-mansour/) and achieved 7 trophies including 3 premier league title. There has been high interests and various opinions in financial spending and performance of teams in Premier League. 
 
-# Data and Source
+> "Money makes the World go round and thus, also the Soccer?" Benedikt Droste, Toward Data Science
 
+> "Premier League clubs ranked by highest average salaries" - NBCSports.com
+
+> Premier League Wages Are A Disgrace -- Except When Your Team's Winning  - Bobby McMahon, Forbes
+
+### Goal
+There has been several statistical analysis between the team spending on salaries&transfers and it's performance in the league. In advance, I got more interested in the team's acquired point and goal for each match compared with the average salary of players actually involved in the match and like to establish the prediction of goals based on the feature.
+
+## Data and Source
 Data in Subject be loaed into pandas DataFrame initially with the following columns in interests.
+Each row represent a game match in the premiere league fixture of each season. The data contanins 380 matchs x 4 seasons.
 
-----------------   -----------------------
- Column              Description
-----------------   -----------------------
-season               Premier League Season from 2015-2016 to 2018-2019
+|Column    |Description
+|---    |---
+|season | Premier League Season from 2015-2016 to 2018-2019
+|date   | Match Date                 
+|attendance | # of attendance for the match
+|squad_a    | The name of Home team
+|squad_a    | The name of Away team
+|goal_a     | Goal win by Home
+|goal_b     | Goal win by Away
+|squad_a_salary | The average salary of player in the match for Home
+|squad_b_salary | The average salary of player in the match for Away
+|xg_a   | The goal prediction for Home by statsbomb.com for 2018-2918 and 2017-2018 seasons
+|xg_b   | The goal prediction for Home by statsbomb.com
 
-date                 
-
-attendance
-
-squad_a
-
-goal_a
-
-goal_b
-
-squad_a_salary
-
-squad_b_salary
-
-xg_a
-
-xg_b
-----------------   -----------------------
-
-
+### Data source
 * https://www.spotrac.com/epl/rankings/ provides EPL Player Salary & Transfer Data for each season. Webscrapping. 
 * https://fbref.com/en/comps/9/1889/schedule/2018-2019-Premier-League-Fixtures provide Premier League fixture & score data including player roll for each match. Webscrapping. 
+
+## Exploratory Data Analysis
+### First, teams performance for overall season based on salary.
+
+#### The correlation of the average team salary and the total point per each season.
+|Season    |Correlation Value
+|---    |---
+|All    |0.83
+|2018_2019  |0.84
+|2017_2018  |0.83
+|2016_2017  |0.77
+|2015_2016  |0.57
+
+#### The correlation of the average team salary and the average goal advantages for each game per season.
+|Season    |Correlation Value
+|---    |---
+|All    |0.85
+|2018_2019  |0.82
+|2017_2018  |0.83
+|2016_2017  |0.76
+|2015_2016  |0.66
+
+As I expected there is strong correlation between the average team salary and the performance of team, the following scatter and regression plot also show the strong trends. 
+
+#### Pair Plot between the team average salary and the total point for all 4 seasons. 
+<p align="center">
+  <img src="images/pairplot_team_salary_point_season_all.png" width = 400>
+</p>
+
+However, the correlation value from the season 2015-2016 is outlier from other season. The reason would be distrupted winning result of Leister City. Liester City finished top of season 2015-2106 with finance in budget. 
+The average salary deployed in each game is 3.37 million pounds(7th), but acquired 81 points in total for the season.
+
+#### The top five team in the average salary in the season 2015-2016
+<p align="center">
+  <img src="images/Bar_the_top5_team_salary_2015_2016.png">
+</p>
+
+#### The top five team in the total point in the season 2015-2016
+<p align="center">
+  <img src="images/Bar_the_top5_point_2015_2016.png">
+</p>
+
+#### The table show top 7 in team salary.
+|Squad  |mean_salary(in Mlb)    |total_point    |Rank
+|---    |---    |---    |---
+|Manchester City  |7.02      |66    |5
+|Manchester Utd   |6.05      |66    |4
+|Chelsea          |5.22      |50    |10
+|Arsenal          |4.43      |71    |2
+|Liverpool        |3.80      |60    |8
+|Tottenham        |3.43      |70    |3
+|Leicester City   |3.37      |81    |1
+
+### Second, how the team salary affect the peformance in each match.
+
+For each match, I added two more columns to compare the salary and Goal. Salary Delta is the salary difference between Home team and Away team. Goal Delta is the goal difference. 
+
+The correlation between Salary Delta and Goal Delta is 0.42. It is not strong value, but we can still observe decent relation from the box plot.
+
+<p align="center">
+  <img src="images/boxplot_goal_delta_salary_delta.png">
+</p>
+
+Since we have the goal prediction for each match in the season 2018-2019 from statsbomb.com, I tried to compare it with the prediction performance of linear regression based on only one feature, the salary delta.
+
+Train Data is the match data from the combined season from 2015-2016, 2016-2017, and 2017-2018.
+Test Data is the match data from the seaon 2018-2019.
+
+I applied the training of data separately for Home team and Away team to predict the goal, because there is home advantage in the match.
+
+#### Root Mean Square Error 
+|Side |Salary Delta | Statsbomb
+|---    |---    |---
+|Home   |1.48   |1.14
+|Away   |1.20   |0.96
+
+#### Error Distribution shows Statsboms has mean 0.90 and std 0.49, Salary Delta has 1.02 and 0.55. 
+<p align="center">
+  <img src="images/RMSE_distribution_comparison.png">
+</p>
+
+## Conclusion
+Even though the correlation between the team salary and the performance in each match is not strong enough to assert, the salary may be the most significant feature to predict the outcome of the game. We could produce a decent prediction model only depends on the salary after all. And, we also could see that it is strongly correlated with overall teams performance in the season also.  
--- src/analyze.py
+++ src/analyze.py
@@ -4,7 +4,6 @@ import numpy as np
 import re
 from decimal import Decimal
 from collections import defaultdict
-
 from tool import fixturedata
 from tool import namematch
 
@@ -12,10 +11,15 @@ import importlib
 importlib.reload(fixturedata)
 
 import matplotlib.pyplot as plt
-plt.style.use('classic')
-%matplotlib inline
+plt.style.use('ggplot')
+#%matplotlib inline
 import seaborn as sns
 sns.set()
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
 
 
 def f_point_squad_a(row):
@@ -38,106 +42,245 @@ def f_point_squad_b(row):
 
 def populate_column(df):
     #win, loss, draw
-    df['squad_a_win'] = df.goal_a > df.goal_b
-    df['squad_b_win'] = df.goal_a < df.goal_b
-    df['squad_draw'] = df.goal_a == df.goal_b
+    df['a_win'] = df.goal_a > df.goal_b
+    df['b_win'] = df.goal_a < df.goal_b
+    df['draw'] = df.goal_a == df.goal_b
     
     #point 3, 0, 1
-    df['squad_a_point'] = df.apply(f_point_squad_a, axis=1)
-    df['squad_b_point'] = df.apply(f_point_squad_b, axis=1)
+    df['a_point'] = df.apply(f_point_squad_a, axis=1)
+    df['b_point'] = df.apply(f_point_squad_b, axis=1)
 
     #salary delta
-    df['squad_a_delta'] = df['squad_a_salary'] - df['squad_b_salary']
-    df['squad_b_delta'] = df['squad_b_salary'] - df['squad_a_salary']
+    df['a_salary_delta'] = df['squad_a_salary'] - df['squad_b_salary']
+    df['b_salary_delta'] = df['squad_b_salary'] - df['squad_a_salary']
+    df = df.astype({'squad_a_salary':'int64', 'squad_b_salary':'int64', 'a_salary_delta':'int64', 'b_salary_delta':'int64'})
 
-    #change salary column to int
-    df = df.astype({'squad_a_salary':'int64', 'squad_b_salary':'int64', 'squad_a_delta':'int64', 'squad_b_delta':'int64'})
+    #goal delta
+    df['a_goal_delta'] = df['goal_a'] - df['goal_b']
+    df['b_goal_delta'] = df['goal_b'] - df['goal_a']
 
     return df
 
 
 def fold_df(df):
 
-    df_a = df[["date", "squad_a", "squad_a_salary", "squad_a_delta",  "goal_a", "goal_b", "squad_a_point"]]
-    df_b = df[["date", "squad_b", "squad_b_salary", "squad_b_delta",  "goal_b", "goal_a", "squad_b_point"]]
+    df_a = df[["date", "squad_a", "squad_a_salary", "a_salary_delta",  "goal_a", "goal_b", "a_goal_delta", "a_point", 'season']]
+    df_b = df[["date", "squad_b", "squad_b_salary", "b_salary_delta",  "goal_b", "goal_a", "b_goal_delta", "b_point", 'season']]
 
-    df_a = df_a.rename(columns = {"squad_a": "squad_name", "squad_a_salary": "squad_salary", "squad_a_delta": "squad_delta", "goal_a": "goal_win", "goal_b": "goal_loss", "squad_a_point":"squad_point"})
-    df_b = df_b.rename(columns = {"squad_b": "squad_name", "squad_b_salary": "squad_salary", "squad_b_delta": "squad_delta", "goal_b": "goal_win", "goal_a": "goal_loss", "squad_b_point":"squad_point"})
+    df_a = df_a.rename(columns = {"squad_a": "squad", "squad_a_salary": "salary", "a_salary_delta": "salary_delta", "goal_a": "goal_win", "goal_b": "goal_loss", "a_goal_delta": "goal_delta", "a_point":"point"})
+    df_b = df_b.rename(columns = {"squad_b": "squad", "squad_b_salary": "salary", "b_salary_delta": "salary_delta", "goal_b": "goal_win", "goal_a": "goal_loss", "b_goal_delta": "goal_delta", "b_point":"point"})
+
+    df_a['home'] = True
+    df_b['home'] = False
 
     return df_a.append(df_b)
 
-def analyze_by_team(df):
 
-    df = fold_df(df)
 
-    df_team = df.groupby("squad_name").agg(
-        average_salary=pd.NamedAgg(column='squad_salary', aggfunc=np.mean),
-        average_delta=pd.NamedAgg(column='squad_delta', aggfunc=np.mean),
-        average_goal_win=pd.NamedAgg(column='goal_win', aggfunc=np.mean),
-        average_goal_loss=pd.NamedAgg(column='goal_loss', aggfunc=np.mean),
-        total_squad_point=pd.NamedAgg(column='squad_point', aggfunc=np.sum)
+def groupby_team(df):
+
+    df_team = df.groupby("squad").agg(
+        mean_salary=pd.NamedAgg(column='salary', aggfunc=np.mean),
+        mean_salary_delta=pd.NamedAgg(column='salary_delta', aggfunc=np.mean),
+        total_goal_win=pd.NamedAgg(column='goal_win', aggfunc=np.sum),
+        total_goal_loss=pd.NamedAgg(column='goal_loss', aggfunc=np.sum),
+        mean_goal_delta=pd.NamedAgg(column="goal_delta", aggfunc=np.mean),
+        total_point=pd.NamedAgg(column='point', aggfunc=np.sum)
         )
     
-    print(df_team.corr())
-
-
-
-    #KDE plot between salary and point
-    with sns.axes_style('white'):
-        sns.jointplot("average_salary", "total_squad_point", df_team, kind='kde')
-
-    #Regression Analysis
-    with sns.axes_style('white'):
-        sns.jointplot("average_salary", "total_squad_point", df_team, kind='reg')
-
-    #correlation
-    corr_salary_point = df_team['average_salary'].corr(df_team['total_squad_point'])
-    print(f"correlation between Salary and Point: {corr_salary_point}")
-
     return df_team
 
+def correlation_salary_point(season, df):
+
+    with sns.axes_style('white'):
+        plot = sns.jointplot("mean_salary", "total_point", df, kind='reg')
+        plot.savefig(f"../images/pairplot_team_salary_point_season_{season}")
     
+    corr_val = df.mean_salary.corr(df.total_point)
+    print(f"for the season '{season}', The correlation between Average Salary and Total Point earned is {corr_val:0.2f}")
+
+def correlation_salary_goal(season, df):
+    
+    with sns.axes_style('white'):
+        plot = sns.jointplot("mean_salary", "mean_goal_delta", df, kind='reg')
+        plot.savefig(f"../images/pairplot_team_salary_goal_delta_season_{season}")
+
+    corr_val = df.mean_salary.corr(df.mean_goal_delta)
+    print(f"for the season '{season}', The correlation between Average Salary and Average Goal Delta is {corr_val:0.2f}")
+
+def display_top_5_salary(season, df):
+
+    df_sorted = df.sort_values(by="mean_salary", ascending=False)
+    print(df_sorted[['mean_salary', 'mean_goal_delta', 'total_point']].head(5))
+
+    ax = df_sorted.head(5).plot.barh(y="mean_salary")
+    fig = ax.figure
+    fig.savefig(f"../images/Bar_the_top5_team_salary_{season}")
+
+def analyze_by_team(df):
+
+    groupd = df.groupby("season")
+    df_2018 = groupd.get_group("2018_2019")
+    df_2017 = groupd.get_group("2017_2018")
+    df_2016 = groupd.get_group("2016_2017")
+    df_2015 = groupd.get_group("2015_2016")
+
+    dict_df = {"all": df, "2018_2019": df_2018, "2017_2018": df_2017, "2016_2017": df_2016, "2015_2016": df_2015}
+
+    for key, value in dict_df.items():
+        dict_df[key] = fold_df(value)
+    
+    for key, value in dict_df.items():
+        dict_df[key] = groupby_team(value)
+
+    for key, value in dict_df.items():
+        correlation_salary_point(key, value)
+
+    for key, value in dict_df.items():
+        correlation_salary_goal(key, value)
+    
+    for key, value in dict_df.items():
+        display_top_5_salary(key, value)
+
+    analyze_by_team_season_2015(dict_df["2015_2016"])
+
+    return dict_df["2015_2016"]
+
+def analyze_by_team_season_2015(df):
+
+    df_sorted = df.sort_values(by="total_point", ascending=False)
+    print(df_sorted[['mean_salary', 'mean_goal_delta', 'total_point']].head(5))
+
+    ax = df_sorted.head(5).plot.barh(y="total_point")
+    fig = ax.figure
+    fig.savefig(f"../images/Bar_the_top5_point_2015_2016")
 
 
+def analyze_salary_delta_with_goal_delta(df):
+    '''
+    We have to compare only half side of df
+
+    INPUT:
+        - df: DataFrame
+    '''
+    df = fold_df(df).groupby('home').get_group(True)
+    ax = df.boxplot(column='salary_delta', by="goal_delta", vert=False)
+    ax.set_title('Distribution of Salary Delta per Goal Delta')
+    ax.set_xlabel('Salary Delta')
+    ax.set_ylabel('Goal Delta')
+
+    fig = ax.figure
+    #fig.set_size_inches(5,5)
+    fig.tight_layout(pad=1)
+    fig.suptitle('')
+    fig.savefig('../images/boxplot_goal_delta_salary_delta')  
+    
+    with sns.axes_style('white'):
+        plot = sns.jointplot("salary_delta", "goal_delta", df, kind='reg')
+        plot.savefig(f"../images/pair_plot_salary_delta_goal_delta")
+
+    corr_val = df.salary_delta.corr(df.goal_delta)
+    print(f"The correlation between Salary Delta and Goal Delta is {corr_val:0.2f}")
+
+def analyze_salary_delta_with_point(df):
+    '''
+    INPUT:
+        - df: DataFrame
+    '''
+    df = fold_df(df)
+    ax = df.boxplot(column='salary_delta', by="point", vert=False)
+    ax.set_title('Distribution of Salary Delta per Point')
+    ax.set_xlabel('Salary Delta')
+    ax.set_ylabel('Point')
+
+    fig = ax.figure
+    #fig.set_size_inches(5,5)
+    fig.tight_layout(pad=1)
+    fig.suptitle('')
+    fig.savefig('../images/boxplot_salary_delta_point')  
+    
+    with sns.axes_style('white'):
+        plot = sns.jointplot("salary_delta", "point", df, kind='reg')
+        plot.savefig(f"../images/pair_plot_salary_delta_point")
+
+    corr_val = df.salary_delta.corr(df.point)
+    print(f"The correlation between Salary Delta and Point is {corr_val:0.2f}")
+
+def predict_goal(df, X, y):
+    '''
+    predict goal for 2018_2019 season base on the previous 3 years
+    
+    INPUT:
+        df: DataFrame
+        X: list of features
+        y: target
+    OUTPUT
+        numpy.ndarray: prediction
+    '''
+    grouped = df.groupby('season')
+    groups = ['2017_2018', '2016_2017', '2015_2016']    
+    train = pd.concat([grouped.get_group(name) for name in groups])
+    #train = grouped.get_group('2017_2018')
+    test = grouped.get_group('2018_2019')
+
+    X_train = train[X]
+    y_train = train[y]
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    X_test = test[X]
+    y_test = test[y]
+    y_hat = model.predict(X_test)
+
+    print(mean_squared_error(y_test, y_hat))
+    #TODO: should print based on condition Home or Away
+    print(mean_squared_error(test.goal_a, test.xg_a))
+    print(mean_squared_error(test.goal_b, test.xg_b))
+
+    return y_hat.flatten()
+
+def analyze_goal_prediction(a, a_hat, b, b_hat):
+
+    sr_MSE = ((a - a_hat)**2 + (b - b_hat)**2)/2 
+    sr_RMSE = np.sqrt(sr_MSE)
+    
+    plot = sns.kdeplot(sr_RMSE, color='b', shade=True, Label="RMSE Distribution")
+    plt.xlabel('RMSE')
+    plt.ylabel('Error Density')
+
+    print(sr_RMSE.describe())
+
+    return sr_RMSE.mean()
+    
 if __name__=='__main__':
 
-    df = fixturedata.build_fixture_dataframe()
-    #df = pd.read_pickle('./data/fixture.pkl')
+    #df = fixturedata.build_fixture_dataframe("2018")
+    df = pd.read_pickle('../data/fixture.pkl')
 
     df = populate_column(df)
 
-    df_team = analyze_by_team(df)
-    print(df_team.sort_values(by="average_salary").head(5))
+    #for analyze
+    groupd = df.groupby('season')
+    df_2018 = groupd.get_group("2018_2019")
+    df_2017 = groupd.get_group("2017_2018")
+    folded = fold_df(df)
+
+    #First, teams performance for overall season based on salary.
+    #df_team = analyze_by_team(df)
+
+    #Second, how the team salary affect the peformance in each match.
+    # analyze_salary_delta_with_goal_delta(df)
+    # analyze_salary_delta_with_point(df)
     
+    #Goal Prediction Analyze
+    pr_a = predict_goal(df, ['a_salary_delta'], 'goal_a')
+    pr_b = predict_goal(df, ['b_salary_delta'], 'goal_b')
 
-    # df = fold_df(df)
+    df_2018['pr_a'] = pd.Series(pr_a, index=df_2018.index)
+    df_2018['pr_b'] = pd.Series(pr_b, index=df_2018.index)
 
-    # #Manchester City
-    # df_man = df[df.squad_name == "Manchester City"]    
-    
-    # #histogram of salary
-    # plt.hist(df_man.squad_salary)
+    RMSE_xg = analyze_goal_prediction(df_2018.goal_a, df_2018.xg_a, df_2018.goal_b, df_2018.xg_b)
+    print(RMSE_xg)
+    # RMSE_pr = analyze_goal_prediction(df_2018.goal_a, df_2018.pr_a, df_2018.goal_b, df_2018.pr_b)
+    # print(RMSE_pr)
 
-
-
-    # df_man_salary_point = df_man[["squad_salary", "squad_point"]]
-    
-
-    # #KDE plot for salary and point
-    # with sns.axes_style('white'):
-    # sns.jointplot("squad_salary", "squad_point", df_man, kind='kde');
-    
-    # #KDE plot for delta and point
-    # with sns.axes_style('white'):
-    # sns.jointplot("squad_delta", "squad_point", df_man, kind='hex')
-
-    # sns.pairplot(df, size=2.5)
-
-    # #why squad_delta strictly normaly distributed
-    # with sns.axes_style('white'):
-    # sns.jointplot("squad_delta", "squad_point", df, kind='reg')
-
-    # #time series
-    # df_man.set_index('date', inplace=True)
-
-#df_c = df[['goal_a', 'goal_b', 'squad_a_win', 'squad_b_win', 'squad_draw', 'squad_a_salary', 'squad_b_salary']]
